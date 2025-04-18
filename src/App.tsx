68903165fce411974ae612d7e7d26e34e14767b0
JEISON101 <reyesjeison2006@gmail.com>
2025-04-17 19:33:30 -0500

    Haciendo landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home/Home'
+import {Home} from './pages/home/Home'
 import SideBar from './components/navegation/SideBar'
 
 function App() {
