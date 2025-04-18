99892d91de50d45ebd7108f7fffc7ab870800bec
IngAlim2023 <enrique.porras@udea.edu.co>
2025-04-17 19:44:07 -0500

    Ruta a login realizada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import {Home} from './pages/home/Home'
 import SideBar from './components/navegation/SideBar'
+import Login from './pages/login/Login'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <SideBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
 
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login:React.FC = () => {
+  return (
+    <div>
+      Hola este es el login
+    </div>
+  )
+}
+
+export default Login
