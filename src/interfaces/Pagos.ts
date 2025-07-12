038fb5bf6e3935592077b94f2c9229de0468ab7d
JEISON101 <reyesjeison2006@gmail.com>
2025-07-12 15:33:49 -0500

    vista de pagos y formulario de solicitudes

---
+++ src/interfaces/Pagos.ts
@@ -0,0 +1,33 @@
+export interface PagoInterface {
+  idPago: number;
+  imagen?: string;
+  noRecibo: number;
+  noContrato: number;
+  titular: string;
+  cobrador: string;
+  fechaCobro: Date;
+  fechaInicio: Date;
+  fechaFin: Date;
+  formaPago: string;
+  monto: number;
+}
+
+export interface ColDataTablePagos {
+  name: string;
+  selector?: (row: any) => string;
+  sortable?: boolean;
+  cell?: (row:PagoInterface) => any;
+  ignoreRowClick?: boolean;
+  allowOverflow?: boolean;
+  button?: boolean;
+}
+
+export interface PostPagoInterface {
+  imagen?: string;
+  titular: number;
+  fechaCobro: Date;
+  fechaInicio: Date;
+  fechaFin: Date;
+  formaPago: number;
+  monto: number;
+}
