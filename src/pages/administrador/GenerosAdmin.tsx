b987ffe31e3183964f3c6141f373f7457bcc624c
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-07-22 09:53:08 -0500

    Historia de usuario generos terminada

--- src/pages/administrador/GenerosAdmin.tsx
+++ src/pages/administrador/GenerosAdmin.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+import FormularioGeneros from "../../components/Administrador/generos/FormularioGeneros";
+import DataTableGeneros from "../../components/Administrador/generos/DataTableGeneros";
 
 const GenerosAdmin:React.FC = () => {
   return (
-    <>
-      
-    </>
+    <div className="p-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap">
+        <FormularioGeneros/>
+        <DataTableGeneros/>
+      </div>
+    </div>
   )
 }
 
