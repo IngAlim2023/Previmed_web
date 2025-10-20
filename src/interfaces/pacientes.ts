7a1db657a831a672116f772a532789d71623363d
JEISON101 <reyesjeison2006@gmail.com>
2025-10-20 16:14:42 -0500

    flujo de registro de pacientes

--- src/interfaces/pacientes.ts
+++ src/interfaces/pacientes.ts
@@ -1,19 +1,10 @@
 export interface Paciente {
-  id: number;
-  nombre: string;
-  segundo_nombre?: string;
-  apellido?: string;
-  segundo_apellido?: string;
-  email?: string;
-  telefono?: string;
-  direccion?: string;
-  numero_documento?: string;
-  fecha_nacimiento?: string;
-  numero_hijos?: number;
-  estrato?: number;
-  autorizacion_datos?: boolean;
-  habilitar?: boolean;
-  doctor: string;
-  plan: string;
-  usuarioId?: string;
+  idPaciente: number;
+  direccionCobro: string;
+  ocupacion: string;
+  activo: boolean;
+  beneficiario: boolean;
+  pacienteId: number;
+  usuarioId: string;
+  usuario: any;
 }
