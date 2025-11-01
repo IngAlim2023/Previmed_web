b550222a0e185d65b7219e8cd016b081aa0722bb
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-10-31 21:52:33 -0500

    Test terminados

---
+++ src/components/planes/DetallesPlan.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { test, expect, vi } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import DetallesPlan from "./DetallesPlan";
+import { Plan } from "../../interfaces/planes";
+
+test("renderiza detalles del plan con beneficios", () => {
+  const planMock: Plan = {
+  idPlan: 1,
+  tipoPlan: "Básico",
+  descripcion: "Plan de prueba",
+  precio: "100000",
+  cantidadBeneficiarios: 2,
+  estado: true
+}
+
+
+  const setShowDetalles = vi.fn();
+
+  render(<DetallesPlan plan={planMock} setShowDetalles={setShowDetalles} />);
+
+  expect(screen.getByText("Detalles del Plan")).toBeInTheDocument();
+  expect(screen.getByText("ID:")).toBeInTheDocument();
+  expect(screen.getByText("1")).toBeInTheDocument();
+  expect(screen.getByText("Tipo de Plan:")).toBeInTheDocument();
+  expect(screen.getByText("Básico")).toBeInTheDocument();
+  expect(screen.getByText("Precio:")).toBeInTheDocument();
+  expect(screen.getByText("Beneficios asociados:")).toBeInTheDocument();
+});
